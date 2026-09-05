047d827ceb7d13d62d424cc26d9f9c71bba8293a
bkhalfi5438-debug <b.khalfi5438@uca.ac.ma>
2026-09-05 14:10:23 +0100

    up

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+# ============================================
+# CONFIGURATION DE LA BASE DE DONNÉES
+# Projet : Plateforme Municipale
+# ============================================
+
+DB_HOST = "127.0.0.1"
+DB_USER = "root"
+DB_PASSWORD = ""
+DB_NAME = "municipalite_db"
+DB_PORT = 3306
